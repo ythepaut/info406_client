65711ccd33238ad2534031bb3a58a80c0674e4ca
gashmob <kevin.traini@etu.univ-smb.fr>
2020-02-07 13:37:12 +0100

    Modification de la classe Location : sa méthode getPath() renvoie toujours le chemin, mais rajoute un / (ou \) à la fin, donc plus besoin de le rajouter à chaque fois

--- src/fr/groupe4/clientprojet/utils/Location.java
+++ src/fr/groupe4/clientprojet/utils/Location.java
@@ -1,20 +1,27 @@
 package fr.groupe4.clientprojet.utils;
 
+import java.io.FileNotFoundException;
 import java.util.StringTokenizer;
 
 public abstract class Location {
 
     /**
-     * Renvoie le path des .class
+     * Renvoie le répertoire de l'exécutable java
+     * Renvoie "/" si non trouvé
      *
-     * @return : String
+     * @return String : sous la forme "blabla/"
      */
     public static String getPath() {
         StringTokenizer stringTokenizer = new StringTokenizer(System.getProperty("java.class.path", "." ), System.getProperty("path.separator"));
-        if (stringTokenizer.hasMoreTokens()) {
-            return stringTokenizer.nextToken();
-        } else {
-            return "";
+        try {
+            if (stringTokenizer.hasMoreTokens()) {
+                return stringTokenizer.nextToken() + System.getProperty("file.separator");
+            } else {
+                throw new FileNotFoundException();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
         }
+        return System.getProperty("file.separator");
     }
 }
