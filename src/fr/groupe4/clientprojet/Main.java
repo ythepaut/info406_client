5c469929fcfd6e0ab80e6fc3ec5740b542f72a46
Romain TOD <39590270+Rominos111@users.noreply.github.com>
2020-02-20 11:31:24 +0100

    Oubli d'un import

--- src/fr/groupe4/clientprojet/Main.java
+++ src/fr/groupe4/clientprojet/Main.java
@@ -1,6 +1,6 @@
 package fr.groupe4.clientprojet;
 
-import fr.groupe4.clientprojet.mainwindow.MainWindow;
+import fr.groupe4.clientprojet.display.mainwindow.view.MainWindow;
 
 public class Main {
 
