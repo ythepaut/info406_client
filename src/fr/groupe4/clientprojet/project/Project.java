cb1c48c9f54b01533da741e182f89dac6ebfd4e5
Rominos111 <39590270+Rominos111@users.noreply.github.com>
2020-02-26 09:49:02 +0100

    Objets Project et ProjectList

---
+++ src/fr/groupe4/clientprojet/project/Project.java
@@ -0,0 +1,24 @@
+package fr.groupe4.clientprojet.project;
+
+import fr.groupe4.clientprojet.project.enums.ProjectStatus;
+
+public class Project {
+    private long id;
+    private String name;
+    private String description;
+    private long deadline;
+    private ProjectStatus status;
+
+    public Project(long id, String name, String description, long deadline, String status) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.deadline = deadline;
+        this.status = ProjectStatus.fromString(status);
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + name + " - " + description + " - " + deadline + " - " + status;
+    }
+}
---
+++ src/fr/groupe4/clientprojet/project/ProjectList.java
@@ -0,0 +1,6 @@
+package fr.groupe4.clientprojet.project;
+
+import java.util.ArrayList;
+
+public class ProjectList extends ArrayList<Project> {
+}
