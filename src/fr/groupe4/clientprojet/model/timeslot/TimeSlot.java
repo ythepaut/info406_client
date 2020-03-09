ffabd1be3e9020a9871691546c75d58f59c2abdd
Romain <romain.theodet@hotmail.fr>
2020-03-09 12:31:38 +0100

    Créneau multiple d'un temps minimal

--- src/fr/groupe4/clientprojet/model/timeslot/TimeSlot.java
+++ src/fr/groupe4/clientprojet/model/timeslot/TimeSlot.java
@@ -2,9 +2,14 @@ package fr.groupe4.clientprojet.model.timeslot;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZoneId;
 
 public class TimeSlot {
+    public static final int LOWEST_TIME = LocalTime.of(7, 0).toSecondOfDay();
+    public static final int HIGHEST_TIME = LocalTime.of(20, 0).toSecondOfDay();
+    public static final int TIME_SCALE = LocalTime.of(0, 15).toSecondOfDay();
+
     private long id;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
@@ -13,8 +18,8 @@ public class TimeSlot {
 
     public TimeSlot(long id, long startTime, long endTime, long idTask, long roomId) {
         this.id = id;
-        this.startTime = Instant.ofEpochMilli(startTime*1000).atZone(ZoneId.systemDefault()).toLocalDateTime();
-        this.endTime = Instant.ofEpochMilli(endTime*1000).atZone(ZoneId.systemDefault()).toLocalDateTime();
+        this.startTime = Instant.ofEpochMilli((startTime/TIME_SCALE)*TIME_SCALE*1000).atZone(ZoneId.systemDefault()).toLocalDateTime();
+        this.endTime = Instant.ofEpochMilli((endTime/TIME_SCALE)*TIME_SCALE*1000).atZone(ZoneId.systemDefault()).toLocalDateTime();
         this.idTask = idTask;
         this.roomId = roomId;
     }
@@ -25,6 +30,14 @@ public class TimeSlot {
 
     public LocalDateTime getEndTime() {
         return endTime;
+    }
+
+    public boolean sameDay() {
+        return startTime.toLocalDate().isEqual(endTime.toLocalDate());
+    }
+
+    public long getId() {
+        return id;
     }
 
     @Override
