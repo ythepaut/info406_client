38b5a96ee906d25705ff09182afee7e8860259d4
Rominos111 <39590270+Rominos111@users.noreply.github.com>
2020-03-14 16:06:27 +0100

    Javadoc + organisation méthodes + fix mineurs

--- src/fr/groupe4/clientprojet/communication/JsonTreatment.java
+++ src/fr/groupe4/clientprojet/communication/JsonTreatment.java
@@ -2,6 +2,7 @@ package fr.groupe4.clientprojet.communication;
 
 import fr.groupe4.clientprojet.model.task.Task;
 import fr.groupe4.clientprojet.model.task.TaskList;
+import org.jetbrains.annotations.NotNull;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -17,8 +18,6 @@ import fr.groupe4.clientprojet.model.resource.human.User;
 import fr.groupe4.clientprojet.model.timeslot.TimeSlot;
 import fr.groupe4.clientprojet.model.timeslot.TimeSlotList;
 
-import java.awt.*;
-
 /**
  * Traite le JSON de la classe Communication
  */
@@ -29,28 +28,35 @@ final class JsonTreatment {
      */
     private static final JsonTreatment singleton = new JsonTreatment();
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     /**
      * Constructeur vide, utile seulement pour le singleton
      */
     private JsonTreatment() {}
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     /**
-     * Fait quelque chose du contenu de la réponse de l'API <br>
-     * synchronized pour éviter les conflits de threads lors de plusieurs traitements simultanés
+     * Fait quelque chose du contenu de la réponse de l'API
      *
      * @param comm Communication à traiter
      * @param jsonObject Contenu à traiter
      */
-    protected static void doSomethingWithData(Communication comm, Object jsonObject) {
+    static void doSomethingWithData(Communication comm, Object jsonObject) {
         switch (comm.typeOfCommunication) {
             case LOGIN:
                 login(comm, jsonObject);
                 break;
-
             case UPDATE_CONNECTION:
                 updateConnection(comm, jsonObject);
                 break;
 
+            case CREATE_PROJECT:
+                break;
+            case GET_PROJECT:
+                getProject(comm, jsonObject);
+                break;
             case LIST_PROJECTS:
                 listProjects(comm, jsonObject);
                 break;
@@ -58,37 +64,26 @@ final class JsonTreatment {
             case GET_USER_INFOS:
                 getUserInfos(comm, jsonObject);
                 break;
-
             case GET_HUMAN_RESOURCE:
                 getHumanResource(comm, jsonObject);
                 break;
 
-            case CREATE_PROJECT:
-                createProject(comm, jsonObject);
+            case CREATE_TASK:
                 break;
-
-            case LIST_MESSAGES:
-                listMessages(comm, jsonObject);
-                break;
-
-            case GET_TIME_SLOT_LIST:
-                getTimeSlotList(comm, jsonObject);
-                break;
-
-            case ADD_TIME_SLOT:
-                addTimeSlot(comm, jsonObject);
-                break;
-
             case GET_TASK_LIST:
                 getTaskList(comm, jsonObject);
                 break;
 
-            case SEND_MESSAGE:
-                sendMessage(comm, jsonObject);
+            case ADD_TIME_SLOT:
+                break;
+            case GET_TIME_SLOT_LIST:
+                getTimeSlotList(comm, jsonObject);
                 break;
 
-            case GET_PROJECT:
-                getProject(comm, jsonObject);
+            case SEND_MESSAGE:
+                break;
+            case LIST_MESSAGES:
+                listMessages(comm, jsonObject);
                 break;
 
             default:
@@ -96,6 +91,52 @@ final class JsonTreatment {
                 break;
         }
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Connexion
+     *
+     * @param comm Communication à traiter
+     * @param jsonObject Contenu à traiter
+     */
+    private static void login(Communication comm, Object jsonObject) {
+        if (comm.status.equals(CommunicationStatus.STATUS_SUCCESS)) {
+            JSONObject jsonContent = (JSONObject) jsonObject;
+
+            JSONObject jsonRequestToken = (JSONObject) jsonContent.get("requests-token");
+            Communication.setRequestToken(singleton, (String) jsonRequestToken.get("value"));
+
+            JSONObject jsonRenewToken = (JSONObject) jsonContent.get("renew-token");
+            Communication.setRenewToken(singleton, (String) jsonRenewToken.get("value"));
+
+            Communication.builder().getUserInfos().startNow().sleepUntilFinished().build();
+        }
+    }
+
+    /**
+     * Mise à jour de la connexion
+     *
+     * @param comm Communication à traiter
+     * @param jsonObject Contenu à traiter
+     */
+    private static void updateConnection(Communication comm, Object jsonObject) {
+        if (comm.htmlCode == HTMLCode.HTML_OK) {
+            JSONObject jsonContent = (JSONObject) jsonObject;
+
+            JSONObject jsonTokenContent = (JSONObject) jsonContent.get("requests-token");
+
+            Communication.setRequestToken(singleton, (String) jsonTokenContent.get("value"));
+        }
+        else if (comm.htmlCode == HTMLCode.HTML_FORBIDDEN) {
+            Logger.error("Update interdite !?");
+        }
+        else {
+            Logger.error("Update malformée !?");
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     private static void getProject(Communication comm, Object jsonObject) {
         if (comm.status == CommunicationStatus.STATUS_SUCCESS) {
@@ -114,7 +155,95 @@ final class JsonTreatment {
         }
     }
 
-    private static void sendMessage(Communication comm, Object jsonObject) {}
+    /**
+     * Liste les projets
+     *
+     * @param comm Communication à traiter
+     * @param jsonObject Contenu à traiter
+     */
+    private static void listProjects(Communication comm, Object jsonObject) {
+        JSONObject jsonContent = (JSONObject) jsonObject;
+        JSONArray projects = (JSONArray) jsonContent.get("projects");
+
+        ProjectList projectsArray = new ProjectList();
+
+        for (Object projectObject : projects) {
+            JSONObject jsonProjectSet = (JSONObject) projectObject;
+            Object[] keySet = jsonProjectSet.keySet().toArray();
+            String key = String.valueOf(keySet[0]);
+
+            JSONObject jsonProject = (JSONObject) jsonProjectSet.get(key);
+
+            Project project = new Project(
+                    (long) jsonProject.get("id"),
+                    (String)jsonProject.get("name"),
+                    (String) jsonProject.get("description"),
+                    (long) jsonProject.get("deadline"),
+                    (String) jsonProject.get("status"));
+
+            projectsArray.add(project);
+        }
+
+        comm.communicationResult = projectsArray;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Récupère les infos de l'utilisateur
+     *
+     * @param comm Communication à traiter
+     * @param jsonObject Contenu à traiter
+     */
+    private static void getUserInfos(Communication comm, Object jsonObject) {
+        if (comm.htmlCode == HTMLCode.HTML_OK) {
+            JSONObject jsonContent = (JSONObject) jsonObject;
+            JSONObject jsonDataContent = (JSONObject) jsonContent.get("data");
+            JSONObject jsonControlContent = (JSONObject) jsonDataContent.get("control");
+            JSONObject jsonUserContent = (JSONObject) jsonDataContent.get("user");
+
+            Communication c = Communication
+                    .builder()
+                    .startNow()
+                    .sleepUntilFinished()
+                    .getHumanResource((long) jsonUserContent.get("id_h_resource"))
+                    .build();
+
+            HumanResource humanResource = (HumanResource) c.getResult();
+
+            comm.communicationResult = new User(humanResource,
+                    (String) jsonControlContent.get("ip"),
+                    (String) jsonControlContent.get("type"),
+                    (long) jsonUserContent.get("id"),
+                    (String) jsonUserContent.get("username"),
+                    (String) jsonUserContent.get("email"));
+        }
+        else {
+            Logger.error("Déconnecté en cours de route ?");
+        }
+    }
+
+    /**
+     * Récupère une ressource humaine
+     *
+     * @param comm Communication à traiter
+     * @param jsonObject Contenu à traiter
+     */
+    private static void getHumanResource(Communication comm, Object jsonObject) {
+        if (comm.status.equals(CommunicationStatus.STATUS_SUCCESS)) {
+            JSONObject jsonContent = (JSONObject) jsonObject;
+
+            comm.communicationResult = new HumanResource(
+                    (long) jsonContent.get("id"),
+                    (String) jsonContent.get("firstname"),
+                    (String) jsonContent.get("lastname"),
+                    (String) jsonContent.get("job"),
+                    (String) jsonContent.get("role"),
+                    (String) jsonContent.get("description"));
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /**
      * Récupère la liste des tâches
@@ -154,13 +283,7 @@ final class JsonTreatment {
         }
     }
 
-    /**
-     * Ajoute un créneau
-     *
-     * @param comm Communication à traiter
-     * @param jsonObject Contenu à traiter
-     */
-    private static void addTimeSlot(Communication comm, Object jsonObject) {}
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /**
      * Récupère une liste de créneaux
@@ -245,141 +368,5 @@ final class JsonTreatment {
 
             comm.communicationResult = messages;
         }
-    }
-
-    /**
-     * Connexion
-     *
-     * @param comm Communication à traiter
-     * @param jsonObject Contenu à traiter
-     */
-    private static void login(Communication comm, Object jsonObject) {
-        if (comm.status.equals(CommunicationStatus.STATUS_SUCCESS)) {
-            JSONObject jsonContent = (JSONObject) jsonObject;
-
-            JSONObject jsonRequestToken = (JSONObject) jsonContent.get("requests-token");
-            Communication.setRequestToken(singleton, (String) jsonRequestToken.get("value"));
-
-            JSONObject jsonRenewToken = (JSONObject) jsonContent.get("renew-token");
-            Communication.setRenewToken(singleton, (String) jsonRenewToken.get("value"));
-
-            Communication.builder().getUserInfos().startNow().sleepUntilFinished().build();
-        }
-    }
-
-    /**
-     * Création de projet
-     *
-     * @param comm Communication à traiter
-     * @param jsonObject Contenu à traiter
-     */
-    private static void createProject(Communication comm, Object jsonObject) {}
-
-    /**
-     * Récupère une ressource humaine
-     *
-     * @param comm Communication à traiter
-     * @param jsonObject Contenu à traiter
-     */
-    private static void getHumanResource(Communication comm, Object jsonObject) {
-        if (comm.status.equals(CommunicationStatus.STATUS_SUCCESS)) {
-            JSONObject jsonContent = (JSONObject) jsonObject;
-
-            comm.communicationResult = new HumanResource(
-                    (long) jsonContent.get("id"),
-                    (String) jsonContent.get("firstname"),
-                    (String) jsonContent.get("lastname"),
-                    (String) jsonContent.get("job"),
-                    (String) jsonContent.get("role"),
-                    (String) jsonContent.get("description"));
-        }
-    }
-
-    /**
-     * Récupère les infos de l'utilisateur
-     *
-     * @param comm Communication à traiter
-     * @param jsonObject Contenu à traiter
-     */
-    private static void getUserInfos(Communication comm, Object jsonObject) {
-        if (comm.htmlCode == HTMLCode.HTML_OK) {
-            JSONObject jsonContent = (JSONObject) jsonObject;
-            JSONObject jsonDataContent = (JSONObject) jsonContent.get("data");
-            JSONObject jsonControlContent = (JSONObject) jsonDataContent.get("control");
-            JSONObject jsonUserContent = (JSONObject) jsonDataContent.get("user");
-
-            Communication c = Communication
-                    .builder()
-                    .startNow()
-                    .sleepUntilFinished()
-                    .getHumanResource((long) jsonUserContent.get("id_h_resource"))
-                    .build();
-
-            HumanResource humanResource = (HumanResource) c.getResult();
-
-            comm.communicationResult = new User(humanResource,
-                    (String) jsonControlContent.get("ip"),
-                    (String) jsonControlContent.get("type"),
-                    (long) jsonUserContent.get("id"),
-                    (String) jsonUserContent.get("username"),
-                    (String) jsonUserContent.get("email"));
-        }
-        else {
-            Logger.error("Déconnecté en cours de route ?");
-        }
-    }
-
-    /**
-     * Mise à jour de la connexion
-     *
-     * @param comm Communication à traiter
-     * @param jsonObject Contenu à traiter
-     */
-    private static void updateConnection(Communication comm, Object jsonObject) {
-        if (comm.htmlCode == HTMLCode.HTML_OK) {
-            JSONObject jsonContent = (JSONObject) jsonObject;
-
-            JSONObject jsonTokenContent = (JSONObject) jsonContent.get("requests-token");
-
-            Communication.setRequestToken(singleton, (String) jsonTokenContent.get("value"));
-        }
-        else if (comm.htmlCode == HTMLCode.HTML_FORBIDDEN) {
-            Logger.error("Update interdite !?");
-        }
-        else {
-            Logger.error("Update malformée !?");
-        }
-    }
-
-    /**
-     * Liste les projets
-     *
-     * @param comm Communication à traiter
-     * @param jsonObject Contenu à traiter
-     */
-    private static void listProjects(Communication comm, Object jsonObject) {
-        JSONObject jsonContent = (JSONObject) jsonObject;
-        JSONArray projects = (JSONArray) jsonContent.get("projects");
-
-        ProjectList projectsArray = new ProjectList();
-
-        for (Object projectObject : projects) {
-            JSONObject jsonProjectSet = (JSONObject) projectObject;
-            Object[] keySet = jsonProjectSet.keySet().toArray();
-            String key = String.valueOf(keySet[0]);
-
-            JSONObject jsonProject = (JSONObject) jsonProjectSet.get(key);
-
-            Project project = new Project(
-                    (long) jsonProject.get("id"),
-                    (String)jsonProject.get("name"),
-                    (String) jsonProject.get("description"),
-                    (long) jsonProject.get("deadline"),
-                    (String) jsonProject.get("status"));
-
-            projectsArray.add(project);
-        }
-
-        comm.communicationResult = projectsArray;
     }
 }
