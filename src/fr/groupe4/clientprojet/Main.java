95bdf7ed49a7b9060dbcdd4203d05b2e72bec038
Gashmob <kevintraini73@outlook.fr>
2020-03-20 09:19:32 +0100

    Création dialog premier lancement Permet de saisir l'url sur laquelle on veut se connecter

--- src/fr/groupe4/clientprojet/Main.java
+++ src/fr/groupe4/clientprojet/Main.java
@@ -2,6 +2,7 @@ package fr.groupe4.clientprojet;
 
 import fr.groupe4.clientprojet.communication.Communication;
 import fr.groupe4.clientprojet.display.dialog.connectiondialog.view.ConnectionDialog;
+import fr.groupe4.clientprojet.display.dialog.firstrundialog.view.FirstRunDialog;
 import fr.groupe4.clientprojet.display.mainwindow.view.MainWindow;
 import fr.groupe4.clientprojet.logger.Logger;
 import fr.groupe4.clientprojet.model.parameters.Parameters;
@@ -11,6 +12,10 @@ public class Main {
     public static void main(String[] args) {
         Logger.init();
         Parameters.init();
+
+        if (Parameters.isFirstRun()) {
+            new FirstRunDialog();
+        }
 
         if (!Communication.isConnected()) {
             new ConnectionDialog();
