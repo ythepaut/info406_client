0fc9640b67d3256202eb12f96852208ec39f8b74
Gashmob <kevintraini73@outlook.fr>
2020-03-20 18:37:34 +0100

    Ajout du champs adresse serveur

--- src/fr/groupe4/clientprojet/display/dialog/parametersdialog/controller/EventParametersDialog.java
+++ src/fr/groupe4/clientprojet/display/dialog/parametersdialog/controller/EventParametersDialog.java
@@ -1,11 +1,9 @@
 package fr.groupe4.clientprojet.display.dialog.parametersdialog.controller;
 
 import fr.groupe4.clientprojet.display.dialog.parametersdialog.view.ParametersDialog;
+import fr.groupe4.clientprojet.model.parameters.Parameters;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 /**
  * Le listener du dialog des paramètres
@@ -15,6 +13,10 @@ public class EventParametersDialog extends WindowAdapter implements ActionListen
      * Le dialog des paramètres
      */
     private ParametersDialog source;
+    /**
+     * Variables statiques pour les 3 boutons du dialog
+     */
+    public static final String OK = "ok", CANCEL = "cancel", APPLY = "apply";
 
     /**
      * Le constructeur
@@ -32,6 +34,25 @@ public class EventParametersDialog extends WindowAdapter implements ActionListen
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case OK:
+                applyParameters();
+                source.dispose();
+                break;
 
+            case CANCEL:
+                source.dispose();
+                break;
+
+            case APPLY:
+                applyParameters();
+                break;
+
+            default:
+        }
+    }
+
+    private void applyParameters() {
+        Parameters.setServerUrl(source.getServerUrl());
     }
 }
--- src/fr/groupe4/clientprojet/display/dialog/parametersdialog/view/ParametersDialog.java
+++ src/fr/groupe4/clientprojet/display/dialog/parametersdialog/view/ParametersDialog.java
@@ -3,9 +3,12 @@ package fr.groupe4.clientprojet.display.dialog.parametersdialog.view;
 import fr.groupe4.clientprojet.display.dialog.parametersdialog.controller.EventParametersDialog;
 import fr.groupe4.clientprojet.display.view.draw.DrawDialog;
 import fr.groupe4.clientprojet.display.view.slide.view.Slide;
+import fr.groupe4.clientprojet.model.parameters.Parameters;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 /**
  * Le dialog des paramètres
@@ -15,6 +18,10 @@ public class ParametersDialog extends DrawDialog {
      * Le listener du dialog
      */
     private EventParametersDialog eventParametersDialog;
+    /**
+     * Tous les champs paramètres
+     */
+    private JTextField serverUrlField;
 
     /**
      * Le constructeur
@@ -37,16 +44,39 @@ public class ParametersDialog extends DrawDialog {
     @Override
     protected void drawContent() {
         setLayout(new BorderLayout());
+
         Slide slide = new Slide();
         slide.addSlide(generalPanel(), "Général");
         slide.addSlide(appearancePanel(), "Apparence");
-
         add(slide, BorderLayout.CENTER);
+
+        JPanel bottomPanel = new JPanel(new FlowLayout());
+        JButton okButton = new JButton("OK");
+        okButton.setActionCommand(EventParametersDialog.OK);
+        okButton.addActionListener(eventParametersDialog);
+        bottomPanel.add(okButton);
+        JButton cancelButton = new JButton("Annuler");
+        cancelButton.setActionCommand(EventParametersDialog.CANCEL);
+        cancelButton.addActionListener(eventParametersDialog);
+        bottomPanel.add(cancelButton);
+        JButton applyButton = new JButton("Appliquer");
+        applyButton.setActionCommand(EventParametersDialog.APPLY);
+        applyButton.addActionListener(eventParametersDialog);
+        bottomPanel.add(applyButton);
+        add(bottomPanel, BorderLayout.SOUTH);
     }
 
     private JPanel generalPanel() {
-        JPanel panel = new JPanel();
-        panel.add(new JLabel("Général"));
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = c.gridy = 0;
+
+        JPanel urlPanel = new JPanel(new GridLayout(1, 2));
+        urlPanel.add(new JLabel("Url serveur API : "));
+        serverUrlField = new JTextField(Parameters.getServerUrl());
+        urlPanel.add(serverUrlField);
+        panel.add(urlPanel);
+
         return panel;
     }
 
@@ -54,5 +84,12 @@ public class ParametersDialog extends DrawDialog {
         JPanel panel = new JPanel();
         panel.add(new JLabel("Apparence"));
         return panel;
+    }
+
+    // _.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-.
+    // Getters des valeurs des champs de paramètres
+
+    public String getServerUrl() {
+        return serverUrlField.getText();
     }
 }
