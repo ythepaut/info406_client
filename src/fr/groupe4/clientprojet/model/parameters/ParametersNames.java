e831f280d19f8f64cb378213f3650277740a713c
Gashmob <kevintraini73@outlook.fr>
2020-03-20 10:46:15 +0100

    Petit oubli

--- src/fr/groupe4/clientprojet/model/parameters/ParametersNames.java
+++ src/fr/groupe4/clientprojet/model/parameters/ParametersNames.java
@@ -1,12 +1,16 @@
 package fr.groupe4.clientprojet.model.parameters;
 
+import java.io.Serializable;
+
 /**
  * Enumeration pour les noms des paramètres
  */
-public enum ParametersNames {
+public enum ParametersNames implements Serializable {
     SERVERURL, // String  | Adresse du serveur auquel on se connecte
     FIRSTRUN, //  boolean | Si c'est la première fois qu'on lance l'application
     ;
+
+    private static final long serialVersionUID = 434052303;
 
     ParametersNames() {}
 }
